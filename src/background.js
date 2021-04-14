3204dcfb5719c35049775833026e3f9c71059f86
Alija Sabic <sabic.alija@gmail.com>
2021-04-14 18:55:47 +0200

    Add stylus support, remove airbnb preset, update manifest

--- src/background.js
+++ src/background.js
@@ -1,7 +1,7 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log('Hello from the background');
+browser.runtime.onMessage.addListener((/*request, sender, sendResponse*/) => {
+  console.log("Hello from the background");
 
   browser.tabs.executeScript({
-    file: 'content-script.js',
+    file: "content-script.js"
   });
 });
